a7db33367d80091f3f03931e23f4928ed2918423
Mario Tijerino <mariogotijerino@gmail.com>
2022-01-31 18:20:11 -0700

    Added scripts. Added control radius field

---
+++ add_to_sys_path.py
@@ -0,0 +1,6 @@
+# copy and paste this code into the maya python console in order to access scripts in this folder.
+
+import sys
+print(sys.path)
+
+sys.path.append('C:\\Users\\Mario\\Documents\\1 Repos\\MayaPythonScripts')
